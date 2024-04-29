d1db03c1263ca48a0f5beb2725392d1f08dd8c3d
akashdeore223 <166868519+akashdeore223@users.noreply.github.com>
2024-04-29 11:57:39 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,81 @@
+  <!-- Page Navigation -->
+    <nav class="navbar custom-navbar navbar-expand-lg navbar-dark" data-spy="affix" data-offset-top="20">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/imgs/logo.png" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Creative studio Landing page">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#service">Service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#team">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimonial">Clients</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#blog">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Of Page Navigation -->
+
+    <!-- Page Header -->
+    <header class="header">
+        <div class="overlay">
+            <h6 class="subtitle" style="color: yellow;">One Top-Shop for BEST PHOTO</h6>
+            <h1 class="title" style="color: #FF214F;">Master Photoshop</h1>
+            <label class="subtitle" style="color: #ffffff; font-size: 25px; padding-top: -10px;" >Best Way to Preserve Your Memories</label>
+            <div class="buttons text-center">
+                <a href="#service" class="btn btn-primary rounded w-lg btn-lg my-1" style="background-color: black;">Our Service</a>
+                <a href="#contact" class="btn btn-outline-light rounded w-lg btn-lg my-1">Contact Us</a>
+            </div>              
+        </div>      
+    </header>
+    <!-- End Of Page Header -->
+
+    <!-- Box -->
+   <div class="box text-center" style="background-color: beige;">
+        <div class="box-item">
+            <img src="assets/imgs/photographer.png" style="padding-top: 10px;" height="100"></i>
+            <p></p>
+            <h6 class="box-title" style="font-style: italic;">PhotoShoot</h6>
+            <p></p>
+        </div>
+
+        <div class="box-item">
+            <img src="assets/imgs/edit.png" style="padding-top: 10px;" height="100"></i>
+            <p></p>
+            <h6 class="box-title" style="font-style: italic;">Image Editing</h6>
+
+        </div>
+        <div class="box-item">
+            <img src="assets/imgs/logo.png" style="padding-top: 10px;" height="90"></i>
+            <p></p>
+            <h6 class="box-title" style="font-style: italic;" style="font-size: 26;">Logo Design</h6>
+            <p></p>
+        </div>
+        
+    </div> 
+    <!-- End of Box -->
